ba3ec01154b59b24e5caee1eacd1ed9cce048999
grosiniboss <paultanase1337@gmail.com>
2025-02-16 20:03:47 +0200

    Added wesbos' eslint+prettier setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
 
 console.log('Welcome to your Webpack template!');
+
+// LINT TEST
+function sayHi(name) {
+  const age = 10;
+}
+
+const age = 12;
+
+const stuff = ['cool', 'doberman!!!!!!'];
